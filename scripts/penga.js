00a4c78d90d44ea5fa750054c01cc29ac6fe6891
Rena Lanford <mitasuki@gmail.com>
2013-10-02 00:02:30 +0100

    Made undo button functional

--- scripts/penga.js
+++ scripts/penga.js
@@ -1,30 +1,41 @@
 var started = false;
 var mousex = 0, mousey = 0;
 
+var paths = {
+	pathCount: 0,
+	pointCount: 0,
+	points: [[]]
+};
+
 $(document).ready(initialise);
 
 function initialise()
 {
 	// Get canvas and context
-	window.canvas = $('#penga')[0];
+	window.canvaselement = $('#penga');
+	window.canvas = canvaselement[0];
 	window.context = canvas.getContext("2d");
 
 	context.lineWidth = 3;
 
-	$('#penga').mousemove(mousemove);
-	$('#penga').mousedown(mousedown);
-	$('#penga').mouseup(mouseup);
+	canvaselement.mousemove(mousemove);
+	canvaselement.mousedown(mousedown);
+	canvaselement.mouseup(mouseup);
+
+	$('#undo').click(undo_click);
 }
 
 function mousemove(event)
 {
-	mousex = event.pageX - $('#penga').offset().left;
-	mousey = event.pageY - $('#penga').offset().top;
+	mousex = event.pageX - canvaselement.offset().left;
+	mousey = event.pageY - canvaselement.offset().top;
 
 	if (started)
 	{
+		paths.points[paths.pathCount].push({ x: mousex, y: mousey });
 		context.lineTo(mousex, mousey);
 		context.stroke();
+		paths.pointCount++;
 	}
 }
 
@@ -33,9 +44,43 @@ function mousedown(event)
 	started = true;
 	context.beginPath();
 	context.moveTo(mousex, mousey);
+	paths.pathCount++;
+	paths.points.push([]);
+	paths.points[paths.pathCount].push({x: mousex, y: mousey});
 }
 
 function mouseup(event)
 {
 	started = false;
+}
+
+function undo_click(event)
+{
+	if (paths.pathCount > 0)
+	{
+		paths.points.pop();
+		paths.pathCount--;
+		redraw();
+	}
+}
+
+function redraw()
+{
+	context.clearRect(0, 0, canvaselement.width(), canvaselement.height());
+
+	for (var i = 0; i < paths.points.length; ++i)
+	{
+		if (paths.points[i].length > 0)
+		{
+			context.beginPath();
+			context.moveTo(paths.points[i][0].x, paths.points[i][0].y);
+
+			for (var j = 0; j < paths.points[i].length; ++j)
+			{
+				context.lineTo(paths.points[i][j].x, paths.points[i][j].y);
+			}
+
+			context.stroke();
+		}
+	}
 }
